799e51cdcea59e56c279ad6cb3c519347b95a2c8
MaxorV <andmxv@gmail.com>
2022-03-09 15:29:14 -0300

    Início projeto BookFlix

---
+++ ProjetoLivros/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+
+namespace ProjetoLivros
+{
+    class Program
+    {
+        static LivroRepositorio repositorio = new LivroRepositorio();
+        static void Main(string[] args)
+        {
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+			while (opcaoUsuario.ToUpper() != "X")
+			{
+				switch (opcaoUsuario)
+				{
+					case "1":
+						ListarLivros();
+						break;
+					case "2":
+						InserirLivro();
+						break;
+					case "3":
+						AtualizarLivro();
+						break;
+					case "4":
+						ExcluirLivro();
+						break;
+					case "5":
+						VisualizarLivro();
+						break;
+					case "C":
+						Console.Clear();
+						break;
+					default:
+						throw new ArgumentOutOfRangeException();
+				}
+
+				opcaoUsuario = ObterOpcaoUsuario();
+			}
+
+			Console.WriteLine("Obrigado por utilizar nossos serviços.");
+			Console.ReadLine();
+        }
+
+        private static void ExcluirLivro()
+		{
+			Console.Write("Digite o id do Livro: ");
+			int indiceLivro = int.Parse(Console.ReadLine());
+
+			repositorio.Exclui(indiceLivro);
+		}
+
+        private static void VisualizarLivro()
+		{
+			Console.Write("Digite o id do Livro: ");
+			int indiceLivro = int.Parse(Console.ReadLine());
+
+			var livro = repositorio.RetornaPorId(indiceLivro);
+
+			Console.WriteLine(livro);
+		}
+
+        private static void AtualizarLivro()
+		{
+			Console.Write("Digite o id do Livro: ");
+			int indiceLivro = int.Parse(Console.ReadLine());
+
+			foreach (int i in Enum.GetValues(typeof(Genero)))
+			{
+				Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+			}
+			Console.Write("Digite o gênero entre as opções acima: ");
+			int entradaGenero = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite o Título do Livro: ");
+			string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o Autor do Livro: ");
+			string entradaAutor = Console.ReadLine();
+
+			Console.Write("Esse livro faz parte de uma saga? Digite 1 para SIM e 2 para NÃO: ");
+			int existesaga = int.Parse(Console.ReadLine());
+			string entradaSaga;
+			if (existesaga == 1)
+			{
+				Console.Write("Digite a Saga a qual esse livro pertence: ");
+				entradaSaga = Console.ReadLine();
+			}
+			else
+				entradaSaga = "";
+
+			Console.Write("Digite o Ano de Lançamento do Livro: ");
+			int entradaAno = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite a Descrição do Livro: ");
+			string entradaDescricao = Console.ReadLine();
+
+			Livro atualizaLivro = new Livro(id: indiceLivro,
+										genero: (Genero)entradaGenero,
+                                        autor: entradaAutor,
+										saga: entradaSaga,
+										titulo: entradaTitulo,
+										ano: entradaAno,
+										descricao: entradaDescricao);
+
+			repositorio.Atualiza(indiceLivro, atualizaLivro);
+		}
+        private static void ListarLivros()
+		{
+			Console.WriteLine("Listar livros");
+
+			var lista = repositorio.Lista();
+
+			if (lista.Count == 0)
+			{
+				Console.WriteLine("Nenhum livro cadastrado.");
+				return;
+			}
+
+			foreach (var livro in lista)
+			{
+                var excluido = livro.retornaExcluido();
+                
+				Console.WriteLine("#ID {0}: - {1} {2}", livro.retornaId(), livro.retornaTitulo(), (excluido ? "*Excluído*" : ""));
+			}
+		}
+
+        private static void InserirLivro()
+		{
+			Console.WriteLine("Inserir novo Livro");
+
+			foreach (int i in Enum.GetValues(typeof(Genero)))
+			{
+				Console.WriteLine("{0}-{1}", i, Enum.GetName(typeof(Genero), i));
+			}
+			Console.Write("Digite o gênero entre as opções acima: ");
+			int entradaGenero = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite o Título do Livro: ");
+			string entradaTitulo = Console.ReadLine();
+
+            Console.Write("Digite o Autor do Livro: ");
+			string entradaAutor = Console.ReadLine();
+
+			Console.Write("Esse livro faz parte de uma saga? Digite 1 para SIM ou qualquer outra tecla para continuar: ");
+			int existesaga = int.Parse(Console.ReadLine());
+			string entradaSaga;
+			if (existesaga == 1)
+			{
+				Console.Write("Digite a Saga a qual esse livro pertence: ");
+				entradaSaga = Console.ReadLine();
+			}
+			else
+				entradaSaga = "";
+			Console.Write("Digite o Ano de Lançamento do Livro: ");
+			int entradaAno = int.Parse(Console.ReadLine());
+
+			Console.Write("Digite a Descrição do Livro: ");
+			string entradaDescricao = Console.ReadLine();
+
+			Livro novoLivro = new Livro(id: repositorio.ProximoId(),
+										genero: (Genero)entradaGenero,
+                                        autor: entradaAutor,
+										saga: entradaSaga,
+										titulo: entradaTitulo,
+										ano: entradaAno,
+										descricao: entradaDescricao);
+
+			repositorio.Insere(novoLivro);
+		}
+
+        private static string ObterOpcaoUsuario()
+		{
+			Console.WriteLine();
+			Console.WriteLine("Bem Vindo(a) a BookFlix!!!");
+			Console.WriteLine("Informe a opção desejada:");
+
+			Console.WriteLine("1- Listar Livros");
+			Console.WriteLine("2- Inserir novo Livro");
+			Console.WriteLine("3- Atualizar Livro");
+			Console.WriteLine("4- Excluir Livro");
+			Console.WriteLine("5- Visualizar Livro");
+			Console.WriteLine("C- Limpar Tela");
+			Console.WriteLine("X- Sair");
+			Console.WriteLine();
+
+			string opcaoUsuario = Console.ReadLine().ToUpper();
+			Console.WriteLine();
+			return opcaoUsuario;
+		}
+    }
+}
